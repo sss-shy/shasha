d629b4a083e834bd57909ac9c6a9048d2f22a274
sss-shy <1638315506@qq.com>
2018-12-05 19:41:05 +0800

    v0

--- shashaw/shasha/views.py
+++ shashaw/shasha/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+#首页
+def index(request):
+    return render(request,"index.html")
+
+#登录
+def login(request):
+    return render(request,"login.html")
+
+#注册
+def register(request):
+    return render(request,"register.html")
+
+
+def cart(request):
+    return render(request,"Shopping Cart.html")
