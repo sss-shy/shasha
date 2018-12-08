b64d0e44ac665f5dbf12247633bdfb3287b7cd9b
sss-shy <1638315506@qq.com>
2018-12-08 16:38:28 +0800

    v2

--- shashaw/shashaw/settings.py
+++ shashaw/shashaw/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'shashaw.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'shasha',
+        'USER':'root',
+        'PASSWORD':'123456'
     }
 }
 
